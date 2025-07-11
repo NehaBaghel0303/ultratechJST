124a497ea5bbabab98830e7b72ce210275dc6e15
Neha Baghel <60838683+NehaBaghel0303@users.noreply.github.com>
2025-07-12 01:51:39 +0530

    Remove laravel code to static site

--- src/resources/views/emails/query.blade.php
+++
@@ -1,4 +0,0 @@
-<h2>New Contact Query</h2>
-<p><strong>Name:</strong> {{ $details['name'] }}</p>
-<p><strong>Email:</strong> {{ $details['email'] }}</p>
-<p><strong>Message:</strong> {{ $details['message'] }}</p>
